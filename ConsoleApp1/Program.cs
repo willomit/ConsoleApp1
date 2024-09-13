84de74b8630ab6ec1595f43b52b3eb313a4ce03a
MKA <MKA@A34PC11>
2024-09-13 12:24:27 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("hello");
+Console.BackgroundColor = ConsoleColor.DarkGray;
+Console.WriteLine("world");
+
