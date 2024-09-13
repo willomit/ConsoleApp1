eb7511e16c031ff176f8c751386a2fd553e3c31c
willomit <sashayurchuk@outlook.com>
2024-09-13 12:49:25 +0300

    ggggbgndd shsh

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,8 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-Console.BackgroundColor = ConsoleColor.DarkBlue;
-Console.WriteLine("hello");
-Console.BackgroundColor = ConsoleColor.DarkGray;
-Console.WriteLine("world");
+//Console.BackgroundColor = ConsoleColor.DarkBlue;
+//Console.WriteLine("hello");
+//Console.BackgroundColor = ConsoleColor.DarkGray;
+//Console.WriteLine("world");
 
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+
+Console.WriteLine("ще не вмерла України");
+
+Console.ResetColor();
